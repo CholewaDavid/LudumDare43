8cbe7f73abc49a5470da1859a8e1c0e01227bd1e
Unknown <cholewa.david96@gmail.com>
2018-12-03 21:31:09 +0100

    Another finish

--- LudumDare43/script/tools/toolManager.js
+++ LudumDare43/script/tools/toolManager.js
@@ -1,7 +1,7 @@
 function ToolManager(){
 	this.tool_pliers = new ToolPliers();
 	this.tool_rubber_button = new ToolRubberButton(3);
-	this.tool_light_bulb = new ToolLightBulb(3);
+	this.tool_light_bulb = new ToolLightBulb(1);
 	this.tool_repair_kit = new ToolRepairKit(3);
 }
 
